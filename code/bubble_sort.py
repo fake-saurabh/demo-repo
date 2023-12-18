0aa3df62f4abc0f5a36c4f427682c156028ff170
Saurabh Misra <misra.saurabh1@gmail.com>
2023-12-17 21:59:14 -0800

    modify the bubble sort just a bit

--- code/bubble_sort.py
+++ code/bubble_sort.py
@@ -1,6 +1,6 @@
 def sorter(arr):
     
-    for i in range(len(arr)):
+    for k in range(len(arr)):
         for j in range(len(arr) - 1):
             if arr[j] > arr[j + 1]:
                 temp = arr[j]
